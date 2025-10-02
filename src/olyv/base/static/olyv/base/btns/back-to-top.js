b44b420775d26bbe6dfa2f90db92708a928d99aa
christianwhocodes <hey@christianwhocodes.space>
2025-10-02 22:55:47 +0000

    updates to back-to-top btn

--- src/olyv/base/static/olyv/base/btns/back-to-top.js
+++ src/olyv/base/static/olyv/base/btns/back-to-top.js
@@ -1,21 +1,16 @@
 const btnBackToTop = document.querySelector("#btn_back_to_top");
+btnBackToTop?.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
 
-function toggleScrollTop() {
+function toggleBtnVisibility() {
   if (!btnBackToTop) return;
 
   const shouldShow = window.scrollY > 100;
   btnBackToTop.classList.toggle("opacity-0", !shouldShow);
   btnBackToTop.classList.toggle("invisible", !shouldShow);
-  btnBackToTop.classList.toggle("opacity-100", shouldShow);
-  btnBackToTop.classList.toggle("visible", shouldShow);
 }
 
-if (btnBackToTop) {
-  btnBackToTop.addEventListener("click", (e) => {
-    e.preventDefault();
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  });
-}
-
-window.addEventListener("load", toggleScrollTop);
-document.addEventListener("scroll", toggleScrollTop);
+window.addEventListener("load", toggleBtnVisibility);
+document.addEventListener("scroll", toggleBtnVisibility);
