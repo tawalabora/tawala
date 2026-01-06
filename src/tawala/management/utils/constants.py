b259e1aa8ee7d3d7cfeebbc9541e145386705d1e
Kevin Wasike Wakhisi <christianwhocodes@gmail.com>
2026-01-06 05:48:25 +0300

    Update constants.py

--- src/tawala/management/utils/constants.py
+++ src/tawala/management/utils/constants.py
@@ -4,6 +4,6 @@ TAWALA = "tawala"
 
 UI = "ui"
 
-UI_PATH = Path(__file__).resolve().parent.parent / UI
+UI_PATH = Path(__file__).resolve().parent.parent.parent / UI
 
 DJANGO_SETTINGS_MODULE = f"{TAWALA}.management.settings"
